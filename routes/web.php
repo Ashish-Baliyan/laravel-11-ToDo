b995de14f294075abca48ae897641e9a6e190dd7
Ashish Baliyan <ashish.baliyan@successive.tech>
2025-01-29 22:45:38 +0530

    make a custom middelware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CustomFacadeGreetingController;
+use App\Http\Middleware\CustomMiddelwareName;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +9,8 @@ Route::get('/', function () {
 });
 
 Route::get('/test-custom-facades', [CustomFacadeGreetingController::class, 'show']);
+
+Route::get('/test-custom-middelware', function () {
+    return 'Test Custom Middelware';
+})->middleware(CustomMiddelwareName::class);
 
